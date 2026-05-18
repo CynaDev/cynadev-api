f8fb4751e0e27b5c073739adc24c1e9f4a8a595b
Arthur <arthur@MacBook-Air-de-Arthur.local>
2026-05-18 14:13:51 +0200

    feat(api): add filter by range dates

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -10,6 +10,7 @@ use ApiPlatform\Metadata\GetCollection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Doctrine\Orm\Filter\DateFilter;
 use App\Config\Statuses_enums;
 use App\Repository\OrderRepository;
 use Doctrine\DBAL\Types\Types;

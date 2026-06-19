736f7c99edabe8e98ccce735f56dbdff3eeea530
Johari-ops <joseph.baert@student.junia.com>
2026-06-19 11:33:43 +0200

    fix payment intent for mobile add promocode

--- src/State/PaymentIntentProcessor.php
+++ src/State/PaymentIntentProcessor.php
@@ -13,13 +13,41 @@ class PaymentIntentProcessor implements ProcessorInterface
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): PaymentIntentResource
     {
-        $intent = $this->stripe->paymentIntents->create([
-            'amount'   => $data->amount,
-            'currency' => $data->currency,
-            'automatic_payment_methods' => ['enabled' => true],
-        ]);
+        // Cas avec subscription + promo (mobile)
+        if (!empty($data->stripePriceId) && !empty($data->customerId)) {
+            $subscriptionParams = [
+                'customer'         => $data->customerId,
+                'items'            => [['price' => $data->stripePriceId]],
+                'payment_behavior' => 'default_incomplete',
+                'expand'           => ['latest_invoice.payment_intent'],
+            ];
 
-        $data->clientSecret = $intent->client_secret;
+            // ← Applique le code promo si fourni
+            if (!empty($data->promoCode)) {
+                $promoCodes = $this->stripe->promotionCodes->all([
+                    'code'  => $data->promoCode,
+                    'limit' => 1,
+                ]);
+                if (!empty($promoCodes->data)) {
+                    $subscriptionParams['discounts'] = [
+                        ['promotion_code' => $promoCodes->data[0]->id]
+                    ];
+                }
+            }
+
+            $subscription = $this->stripe->subscriptions->create($subscriptionParams);
+            $data->clientSecret = $subscription->latest_invoice->payment_intent->client_secret;
+
+        } else {
+            // Cas simple sans subscription (ancien comportement)
+            $intent = $this->stripe->paymentIntents->create([
+                'amount'                    => $data->amount,
+                'currency'                  => $data->currency,
+                'automatic_payment_methods' => ['enabled' => true],
+            ]);
+            $data->clientSecret = $intent->client_secret;
+        }
+
         return $data;
     }
 }
